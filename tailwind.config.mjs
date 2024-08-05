74b435fd5e502d3c8bb543e888e5efec2d050ab1
eddieb <eddiewird@gmail.com>
2024-08-05 13:41:20 -0400

    scuffed version of how will it work

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,7 @@ export default {
 				'dino-teal': '#76B8B0',
 				'dino-dark-teal': '#449F94',
 				'dino-neon-orange': '#FDA747',
+				'dino-dark-neon-orange': '#FF8700',
 				'dino-orange': '#FFBE74',
 				'dino-dark-orange': '#E97D04',
 				'light-dino-teal': '#ADD4D0',
